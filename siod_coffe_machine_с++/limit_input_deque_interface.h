1731a3663ce4b079626f9f20ebea47e8efdbfd9c
Дмитрий Помозов <dima.pomozov2003@gmail.com>
2022-06-28 18:02:22 +0300

    array deque

--- siod_coffe_machine_с++/limit_input_deque_interface.h
+++ siod_coffe_machine_с++/limit_input_deque_interface.h
@@ -2,7 +2,7 @@ template <class Data> class dequeInterface
 {
 	virtual int empty() = 0;
 	virtual int full() = 0;
-	virtual int inRight(Data* pSrcData) = 0;
+	virtual int inRight(Data*) = 0;
 	virtual int outLeft() = 0;
 	virtual int outRight() = 0;
 	virtual Data readLeft() = 0;
--- siod_coffe_machine_с++/limited_input_deque_array.h
+++ siod_coffe_machine_с++/limited_input_deque_array.h
@@ -1,1 +1,57 @@
-#pragma once
+#include "limit_input_deque_interface.h"
+
+template <class Data> class deque : public dequeInterface<Data>
+{
+	int m_left, m_right, m_maxLength, m_length;
+	Data* m_data;
+
+	deque(int maxLength = 10)
+	{
+		m_maxLength = maxLength;
+		m_left = 0;
+		m_right = m_maxLength - 1;
+		m_data = new Data[m_maxLength];
+	}
+
+	int empty()
+	{
+		return (m_right + 1) % m_maxLength == m_left;
+	}
+
+	int full()
+	{
+		return (m_right + 2) % m_maxLength == m_left;
+	}
+
+	int inRight(Data* pSrcData)
+	{
+		if (full())
+			return 0;
+		m_right = (m_right + 1) % m_maxLength;
+		m_data[m_right] = *pSrcData;
+		return 1;
+	}
+	Data outRight()
+	{
+		int temp = m_right;
+		m_right = m_right ? m_right - 1 : m_maxLength - 1;
+		return m_data[tmp];
+	}
+
+	Data outLeft()
+	{
+		int temp = m_left;
+		m_left = = (m_left + 1) % m_maxLength;
+		return m_data[m_left];
+	}
+
+	Data readRight()
+	{
+		return m_data[m_right];
+	}
+
+	Data readLeft()
+	{
+		return m_data[m_left];
+	}
+};
